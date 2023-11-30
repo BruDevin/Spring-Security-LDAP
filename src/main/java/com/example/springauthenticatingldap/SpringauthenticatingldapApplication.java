48a4c572f54fb70b73fe58f3813ff41aa5a7559d
BruDevin <bruno.ads.2022@gmail.com>
2023-11-30 15:02:34 -0300

    Spring Security LDAP example

---
+++ src/main/java/com/example/springauthenticatingldap/SpringauthenticatingldapApplication.java
@@ -0,0 +1,13 @@
+package com.example.springauthenticatingldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringauthenticatingldapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringauthenticatingldapApplication.class, args);
+	}
+
+}
